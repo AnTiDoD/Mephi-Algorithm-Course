c94a385b8d0b3da5292620a521216b124c7a029c
Aleksey Taigunov <tajgunov@yandex.ru>
2011-11-26 15:30:35 +0400

    update MKAD

---
+++ Home-Execises2/mkad/mkad.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	FILE* f;
+
+	if ((f=fopen("input.txt","rt"))==NULL)
+	{
+		perror("input.txt");
+		return 1;
+	}
+	int v,t;
+	fscanf(f,"%d%d",&v,&t);
+	fclose(f);
+
+	int nm; //номер отметки километра
+	
+	nm = v>=0?(v*t)%109:109+(v*t)%109;
+
+	f=fopen("output.txt","wt");
+	fprintf(f,"%d",nm);
+	fclose(f);
+
+
+	
+	return 0;
+}
+
