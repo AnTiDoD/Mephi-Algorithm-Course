df99ce1aff4c6cd3c567e73afdbb339cb71aaa30
Aleksey Taigunov <tajgunov@yandex.ru>
2011-11-26 15:30:15 +0400

    update Change

---
+++ Home-Execises2/change/ReadMe.txt
@@ -0,0 +1,6 @@
+﻿Идея решения:
+
+Переводим деньги в копейки, вычитаем и преобразуем обратно
+
+Условие задачи:
+http://informatics.mccme.ru/moodle/mod/statements/view3.php?id=276&chapterid=262#1
---
+++ Home-Execises2/change/change.cpp
@@ -0,0 +1,25 @@
+﻿#include "stdio.h>
+
+int main()
+{
+	FILE* f;
+
+	if ((f=fopen("input.txt","rt"))==NULL)
+	{
+		perror("input.txt");
+		return 1;
+	}
+	int p1,k1,p2,k2; //рубли и копейки
+	fscanf(f,"%d%d%d%d",&p1,&k1,&p2,&k2);
+	fclose(f);
+
+	//сдача	в копейках
+	int change = (p2*100+k2) - (p1*100+k1);
+	
+	f=fopen("output.txt","wt");
+	fprintf(f,"%d\n%d",change/100,change%100);
+	fclose(f);
+
+	return 0;
+}
+
